e5cf0523a653c52ba646241c4a13081b8075c86d
McCambley <jakemccamb16@gmail.com>
2022-03-02 22:21:05 -0500

    Dev: complete #12

--- main.sql
+++ main.sql
@@ -388,3 +388,12 @@ SELECT length FROM songs;
 
 SELECT AVG(length) as 'Average Song Duration' 
 FROM songs;
+
+-- 11. Select the longest Song off each Album
+SELECT 
+	albums.name as 'Album', 
+	release_year as 'Release Year', 
+	MAX(songs.length) as 'Duration'
+FROM albums
+JOIN songs ON albums.id = songs.album_id
+GROUP BY songs.album_id;
